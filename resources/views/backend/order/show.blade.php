2193db9f3546f6d43c32ef782bfd029919cfcc15
Bolaji Teslim <bolajiteslim05@gmail.com>
2024-03-19 22:45:17 +0100

    added naira symbol(₦)

--- resources/views/backend/order/show.blade.php
+++ resources/views/backend/order/show.blade.php
@@ -30,7 +30,7 @@
             <td>{{$order->email}}</td>
             <td>{{$order->quantity}}</td>
             <td>${{$order->shipping->price}}</td>
-            <td>${{number_format($order->total_amount,2)}}</td>
+            <td>₦{{number_format($order->total_amount,2)}}</td>
             <td>
                 @if($order->status=='new')
                   <span class="badge badge-primary">NEW</span>
@@ -84,11 +84,11 @@
                     </tr>
                     <tr>
                       <td>Coupon</td>
-                      <td> : $ {{number_format($order->coupon,2)}}</td>
+                      <td> : ₦ {{number_format($order->coupon,2)}}</td>
                     </tr>
                     <tr>
                         <td>Total Amount</td>
-                        <td> : $ {{number_format($order->total_amount,2)}}</td>
+                        <td> : ₦ {{number_format($order->total_amount,2)}}</td>
                     </tr>
                     <tr>
                         <!-- <td>Payment Method</td>
